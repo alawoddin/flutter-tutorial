bc8c813f1063383ca536dd2937b23344c33a5c1d
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-02 12:46:06 +0430

    i add the background color

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello, World!'))),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 88, 1, 104),
+        body: Center(child: Text('Hello, World!')),
+      ),
     ),
   );
 }
