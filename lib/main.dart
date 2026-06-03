01df7301cdbed3d5a60d4942a1a7ab1a3bd57af3
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-03 15:15:15 +0430

    the color and text is done

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 import 'gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 73, 8, 185),
+          Color.fromARGB(255, 158, 10, 184),
+        ),
+      ),
+    ),
+  );
 }
