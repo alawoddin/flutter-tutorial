0b528675a637c65b4fec532a936a32b52836c0f6
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-03 14:23:00 +0430

    i create the new page and call the widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
   runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.deepPurple, Colors.purpleAccent],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-      ),
-    );
-  }
 }
